7f8a38ae76a116cef648b8087035ff8a1b67573f
Craig L <c@MacBook-Pro.local>
2023-08-31 20:10:05 -0500

    progress on sign-in and sign-up pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,13 +11,16 @@ export const metadata = {
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({ children }: {children: React.ReactNode}) {
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
     return (
-    <ClerkProvider>
+      <ClerkProvider>
         <html lang="en">
-            <body className={`${inter.className} bg-dark-1`}>
-                { children }
-            </body>
+          <body className={inter.className}>{children}</body>
         </html>
-    </ClerkProvider>)
-}
+      </ClerkProvider>
+    )
+  }
